8c5be46692512b1e02f92aeb0df5bed8a7e00537
Ganbusier <h.gan@student.tudelft.nl>
2024-08-02 15:19:29 +0800

    update

---
+++ code/pointcloud_process/src/ply_process.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <ply_process.h>
+
+int main(int argc, char* argv[]) {
+    
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << "<input_ply_file> <output_ply_file>" << std::endl;
+    }
+
+    return 0;
+}
