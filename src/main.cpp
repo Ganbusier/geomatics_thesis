4fba72397c7fa46fd15fd6457569d518f33ef96f
Ganbusier <h.gan@student.tudelft.nl>
2024-11-18 16:55:26 +0100

    refractor(main): test AHN pointcloud

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 
 // using namespace rerun::demo;
 
-
 int main(int argc, char** argv) {
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " <input_file_path> <semantic_class>" << std::endl;
@@ -22,12 +21,13 @@ int main(int argc, char** argv) {
 
     // declare point cloud
     pcl::PointCloud<CustomPoint>::Ptr cloud(new pcl::PointCloud<CustomPoint>);
-    loadPLY(cloud, input_file_path, true);
+    loadPLY(cloud, input_file_path, false);
 
     // filter points by semantic class and instance class
     std::unordered_map<int, pcl::PointCloud<CustomPoint>::Ptr> grouped_points;
     filterBySemClass(grouped_points, cloud, semantic_class);
-
+    std::cout << "Number of instances: " << grouped_points.size() << std::endl;
+    
     // For each instance, compute normals and detect shapes using CGAL
     for (const auto& [ins_class, points] : grouped_points) {
         ransac_run(points, ins_class);
